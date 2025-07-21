4b7e747b9f69524baa25ba04377c720bb98d68af
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-07-21 12:18:49 +0900

    Delete back directory

--- back/src/main/java/com/example/_thecore_back/rest/emulator/db/EmulatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example._thecore_back.rest.emulator.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface EmulatorRepository extends JpaRepository<EmulatorEntity, Long> {
-    Optional<EmulatorEntity> findByCarNumber(String carNumber);
-}
